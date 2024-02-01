a178bca3e2482511cf82e3accddaf0c2c47847c3
nnatastysoft <nyinyiaung.analyst@gmail.com>
2024-02-01 11:22:53 +0630

    flutter expense tracker tutorial

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          children: [
+            Text(
+              expense.title, 
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 4,),
+            Row(
+              children: [
+                //{} will take the whole as String value
+                Text('\$${expense.amount.toStringAsFixed(2)}'),//Parameter (2) will show 2 places after the point
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 8,),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
